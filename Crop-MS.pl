949dcb6c7f22b549fa53a3c54b9094e765072cf0
Hansaka2001 <hansakarukshan43@gmail.com>
2024-07-08 14:22:57 +0530

    structure created

---
+++ Crop-MS.pl
@@ -0,0 +1,33 @@
+% Facts about different crop types
+crop_type(wheat).
+crop_type(rice).
+crop_type(corn).
+crop_type(potato).
+crop_type(tomato).
+crop_type(soybean).
+crop_type(barley).
+crop_type(oats).
+crop_type(sunflower).
+crop_type(cotton).
+
+% Facts about different soil types
+soil_type(clay).
+soil_type(sandy).
+soil_type(loam).
+soil_type(silt).
+soil_type(peat).
+soil_type(chalky).
+soil_type(saline).
+soil_type(alluvial).
+
+% Facts about different types of fertilizers
+fertilizer(nitrogen).
+fertilizer(phosphorus).
+fertilizer(potassium).
+fertilizer(calcium).
+fertilizer(magnesium).
+fertilizer(sulfur).
+fertilizer(iron).
+fertilizer(manganese).
+fertilizer(boron).
+fertilizer(zinc).
